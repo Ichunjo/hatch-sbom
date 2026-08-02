2928f8d62c3d8e5df8952084943fe8d642394b90
Vardë <ichunjo.le.terrible@gmail.com>
2026-08-02 16:15:03 +0200

    fix build

--- hatch_build.py
+++ hatch_build.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+from typing import Any
 
 # Add the src directory to the module search path so we can import the local plugin
 src_path = Path(__file__).parent / "src"
@@ -8,5 +9,11 @@ sys.path.insert(0, str(src_path))
 from hatch_sbom.plugin import SbomBuildHook  # noqa: E402
 
 
-def get_build_hook() -> type[SbomBuildHook]:
-    return SbomBuildHook
+class CustomSbomBuildHook(SbomBuildHook):
+    def initialize(self, version: str, build_data: dict[str, Any]) -> None:
+        del self.config["path"]
+        return super().initialize(version, build_data)
+
+
+def get_build_hook() -> type[CustomSbomBuildHook]:
+    return CustomSbomBuildHook
